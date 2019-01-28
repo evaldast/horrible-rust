59879487210be39f644a3f2198d70e8697ff670c
unknown <evaldas.tyla@gmail.com>
2019-01-28 23:27:50 +0200

    Added current season anime title parsing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
+extern crate failure;
+extern crate kuchiki;
+extern crate reqwest;
 extern crate rss;
 extern crate rusqlite;
 
+use failure::Error;
+use kuchiki::traits::*;
+use kuchiki::*;
 use rss::Channel;
 use rusqlite::types::ToSql;
 use rusqlite::{Connection, NO_PARAMS};
@@ -8,6 +14,7 @@ use rusqlite::{Connection, NO_PARAMS};
 #[derive(Debug)]
 struct Anime {
     title: String,
+    subscribed: Option<u8>,
     episodes: Vec<Episode>,
 }
 
@@ -15,7 +22,7 @@ struct Anime {
 enum Resolution {
     Q480p,
     Q720p,
-    Q1080p
+    Q1080p,
 }
 
 #[derive(Debug)]
@@ -24,7 +31,7 @@ struct Episode {
     title: String,
     watched: Option<u8>,
     resolution: Resolution,
-    torrent_link: String,    
+    torrent_link: String,
 }
 
 trait Str {
@@ -36,33 +43,34 @@ impl Str for Resolution {
     fn to_str(&self) -> Result<&'static str, ()> {
         match self {
             Resolution::Q480p => Ok("480p"),
-            Resolution::Q720p => Ok("480p"),
-            Resolution::Q1080p => Ok("480p"),
+            Resolution::Q720p => Ok("720p"),
+            Resolution::Q1080p => Ok("1080p"),
             _ => Err(()),
         }
     }
 
     fn from_str(s: String) -> Result<Resolution, ()> {
-        println!("{}", s);
-
         match s.as_ref() {
             "480p" => Ok(Resolution::Q480p),
             "720p" => Ok(Resolution::Q720p),
-            "1080p"=> Ok(Resolution::Q1080p),
+            "1080p" => Ok(Resolution::Q1080p),
             _ => Err(()),
         }
     }
 }
 
 fn main() {
-    process()
+    match process() {
+        Ok(_) => println!("Success"),
+        Err(e) => println!("{}", e),
+    }
 }
 
-fn process() {
-    let conn = Connection::open_in_memory().unwrap();
+fn process() -> Result<String, Error> {
+    let conn = Connection::open("main.db")?;
 
     conn.execute(
-        "CREATE TABLE episode (
+        "CREATE TABLE episodes (
                   id              INTEGER PRIMARY KEY AUTOINCREMENT,
                   title           TEXT NOT NULL,
                   watched         BOOLEAN DEFAULT 0,
@@ -70,10 +78,9 @@ fn process() {
                   torrent_link    TEXT NOT NULL
                   )",
         NO_PARAMS,
-    )
-    .unwrap();
+    )?;
 
-    let channel = Channel::from_url("https://nyaa.si/?page=rss&c=0_0&f=0&u=HorribleSubs").unwrap();
+    let channel = Channel::from_url("https://nyaa.si/?page=rss&c=0_0&f=0&u=HorribleSubs")?;
 
     for item in channel.items() {
         let title = item.title().unwrap();
@@ -83,36 +90,63 @@ fn process() {
             title: title.to_string(),
             watched: None,
             resolution: parse_resolution_from_title(title),
-            torrent_link: item.link().unwrap().to_string()            
+            torrent_link: item.link().unwrap().to_string(),
         };
 
         conn.execute(
-            "INSERT INTO episode (title, resolution, torrent_link)
+            "INSERT INTO episodes (title, resolution, torrent_link)
                   VALUES (?1, ?2, ?3)",
-            &[&ep.title as &ToSql, &ep.resolution.to_str().unwrap(), &ep.torrent_link]
-        )
-        .unwrap();
+            &[
+                &ep.title as &ToSql,
+                &ep.resolution.to_str().unwrap(),
+                &ep.torrent_link,
+            ],
+        )?;
     }
 
-    let mut stmt = conn
-        .prepare("SELECT id, title, watched, resolution, torrent_link FROM episode")
-        .unwrap();
-
-    let episode_iter = stmt
-        .query_map(NO_PARAMS, |row| Episode {
-            id: row.get(0),
-            title: row.get(1),
-            watched: row.get(2),
-            resolution: Resolution::from_str(row.get(3)).unwrap(),                        
-            torrent_link: row.get(4),
-        })
-        .unwrap();
+    let mut episode_selection =
+        conn.prepare("SELECT id, title, watched, resolution, torrent_link FROM episodes")?;
+    let episode_iter = episode_selection.query_map(NO_PARAMS, |row| Episode {
+        id: row.get(0),
+        title: row.get(1),
+        watched: row.get(2),
+        resolution: Resolution::from_str(row.get(3)).unwrap(),
+        torrent_link: row.get(4),
+    })?;
 
     for episode in episode_iter {
-        println!("Found episode {:?}", episode.unwrap());
+        println!("Found episode {:?}", episode?);
     }
+
+    for title in fetch_current_season_titles()? {
+        println!("Found title {:?}", title);
+    }
+
+    return Ok("".to_string());
 }
 
 fn parse_resolution_from_title(title: &str) -> Resolution {
     Resolution::Q480p
+}
+
+fn fetch_current_season_titles() -> Result<Vec<String>, Error> {
+    let doc = fetch_document("https://horriblesubs.info/current-season/")
+        .ok()
+        .unwrap();
+    let title_selector = doc.select(".shows-wrapper").unwrap();
+    let title_wrapper: NodeDataRef<ElementData> = title_selector.into_iter().next().unwrap();
+    let text_content = title_wrapper.text_contents();
+    let titles: Vec<String> = text_content
+        .lines()
+        .filter(|t| !t.is_empty())
+        .map(|s| s.to_string())
+        .collect();
+
+    Ok(titles)
+}
+
+fn fetch_document(url: &str) -> Result<NodeRef, Error> {
+    let mut res = reqwest::get(url)?;
+
+    Ok(kuchiki::parse_html().one(res.text()?))
 }
